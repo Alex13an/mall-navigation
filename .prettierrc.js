943d39ad5243c66555ef5a1fe416b03c0d0d9aac
Alexandr An <alexandr13an@yandex.ru>
2022-10-19 17:56:15 +0300

    linter configured

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  useTabs: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 150,
+  tabWidth: 2,
+  endOfLine: 'auto',
+  arrowParens: 'always',
+};
